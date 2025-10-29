41f03415a2a8d4840cb5c65f7e2c48b7738f3679
GodNess <136266892+gonhunter7@users.noreply.github.com>
2025-10-29 12:39:40 -0300

    Add files via upload

--- base/pages/03_✅Mostramos en mapas.py
+++ base/pages/03_✅Mostramos en mapas.py
@@ -20,10 +20,6 @@ def generar_mapa():
     )
     return m
 
-lagos = pd.read_csv('lagos_arg.csv')
-st.title("Mapa")
-mapa = generar_mapa()
-
 def agregar_marca_aerop(row):
     folium.Marker(
         [row['lat'], row['lng']],
@@ -31,25 +27,33 @@ def agregar_marca_aerop(row):
         icon=folium.Icon()
     ).add_to(mapa)
 
-ac1,ac2 = st.columns([0.3, 0.7])
-r_areas = ac1.checkbox("Grandes", key="lagos_grandes")
-if r_areas:
-    a_larg = lagos[lagos['Sup Tamaño']=='grande']
-    a_larg.apply(agregar_marca_aerop, axis=1)
-r_parques = ac1.checkbox("Medianos", key="lagos_medianos")
-if r_parques:
-    a_med = lagos[lagos['Sup Tamaño']=='medio']
-    a_med.apply(agregar_marca_aerop, axis=1)
-r_monu = ac1.checkbox("Pequeños", key="lagos_pequeños")
-if r_monu:
-    a_small = lagos[lagos['Sup Tamaño']=='chico']
-    a_small.apply(agregar_marca_aerop, axis=1)
-with ac2:
-    st_folium(mapa, key='lagos')
+tab_lagos, tab_felinos = st.tabs(["Mapa Lagos", "Mapa Felinos"])
+lagos = pd.read_csv('lagos_arg.csv')
+
+with tab_lagos:
+    
+    st.title("Mapa Lagos")
+    
+    st.markdown("Este mapa muestra los lagos de Argentina podiendo filtrar por tamaño")
+    mapa = generar_mapa()
+
+    ac1,ac2 = st.columns([0.3, 0.7])
+    r_areas = ac1.checkbox("Grandes", key="lagos_grandes")
+    if r_areas:
+        a_larg = lagos[lagos['Sup Tamaño']=='grande']
+        a_larg.apply(agregar_marca_aerop, axis=1)
+    r_parques = ac1.checkbox("Medianos", key="lagos_medianos")
+    if r_parques:
+        a_med = lagos[lagos['Sup Tamaño']=='medio']
+        a_med.apply(agregar_marca_aerop, axis=1)    
+    r_monu = ac1.checkbox("Pequeños", key="lagos_pequeños")
+    if r_monu:
+        a_small = lagos[lagos['Sup Tamaño']=='chico']
+        a_small.apply(agregar_marca_aerop, axis=1)
+    with ac2:
+        st_folium(mapa, key='lagos')
 
 felinos = pd.read_csv('felinos_filtrado.csv')
-st.title("Mapa")
-mapa = generar_mapa()
 
 def agregar_marca_aerop(row):
     folium.Marker(
@@ -58,21 +62,29 @@ def agregar_marca_aerop(row):
         icon=folium.Icon()
     ).add_to(mapa)
 
-ac1, ac2 = st.columns([0.3, 0.7])
 
-genus_unicos = felinos['genus'].unique()[:3]
+with tab_felinos:
+    st.title("Mapa Felinos")
 
-r_genus1 = ac1.checkbox(genus_unicos[0], key="felinos_genus1")
-if r_genus1:
-    felinos[felinos['genus'] == genus_unicos[0]].apply(agregar_marca_aerop, axis=1)
+    st.markdown("Este mapa muestra los avistamientos de felinos de Argentina, podiendo filtrar por especie")
+    
+    mapa = generar_mapa()
 
-r_genus2 = ac1.checkbox(genus_unicos[1], key="felinos_genus2")
-if r_genus2:
-    felinos[felinos['genus'] == genus_unicos[1]].apply(agregar_marca_aerop, axis=1)
+    ac1, ac2 = st.columns([0.3, 0.7])
 
-r_genus3 = ac1.checkbox(genus_unicos[2], key="felinos_genus3")
-if r_genus3:
-    felinos[felinos['genus'] == genus_unicos[2]].apply(agregar_marca_aerop, axis=1)
+    genus_unicos = felinos['genus'].unique()[:3]
 
-with ac2:
-    st_folium(mapa, key='felinos')
+    r_genus1 = ac1.checkbox(genus_unicos[0], key="felinos_genus1")
+    if r_genus1:
+        felinos[felinos['genus'] == genus_unicos[0]].apply(agregar_marca_aerop, axis=1)
+
+    r_genus2 = ac1.checkbox(genus_unicos[1], key="felinos_genus2")
+    if r_genus2:
+        felinos[felinos['genus'] == genus_unicos[1]].apply(agregar_marca_aerop, axis=1)
+
+    r_genus3 = ac1.checkbox(genus_unicos[2], key="felinos_genus3")
+    if r_genus3:
+        felinos[felinos['genus'] == genus_unicos[2]].apply(agregar_marca_aerop, axis=1)
+
+    with ac2:
+        st_folium(mapa, key='felinos')
